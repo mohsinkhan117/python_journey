609352a032356cf112761dc7ff6c40d1b81b4746
mohsinkhan117 <mohsinkhanmandan.com>
2026-04-29 10:55:15 -0700

    Basic  functions and syntax done

---
+++ functions/basics.py
@@ -0,0 +1,38 @@
+def hello():
+    print('This is hello function')
+
+
+hello()
+
+def greet_user(name):
+    print(f"Hello, {name}")
+
+greet_user("Mohsin")
+
+
+def sum(a,b):
+    return a+b
+
+print('sum of a and b is: ',sum(3,5))
+
+def exp(base,exp=2): # bydefault set to 2
+    return base **exp
+
+print("exponentially 4 and 2 is ",exp(4))
+print("exponentially 3 and 3 is ",exp(3,3))
+
+
+#arguments are interchangeable by using the keyword
+def student(name, age):
+    print(name, age)
+
+student(age=20, name="Ali")
+
+
+
+#arbitrary numbers of arguments 
+
+def total_sum(*numbers):
+    return sum(numbers)
+
+print(total_sum(1, 2, 3, 4, 5))
