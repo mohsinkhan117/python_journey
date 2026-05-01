c339993c5a1836ed879462158f82ebf3dd38c584
mohsinkhan117 <mohsinkhanmandan@gmail.com>
2026-05-01 12:30:08 -0700

    shape of array

--- data_structures/numpy_arrays.py
+++ data_structures/numpy_arrays.py
@@ -38,3 +38,30 @@ print("Numpy multi dimensional array in python\n",
 print(type(list_1))
 
 print(type(sample_array))
+
+
+# Shape: Number of elements along with each axis and is returned as a tuple
+sample_array = np.array([[0, 4, 2],
+                       [3, 4, 5],
+                       [23, 4, 5],
+                       [2, 34, 5],
+                       [5, 6, 7]])
+
+print("shape of the array :",
+      sample_array.shape)  # shape of the array : (5, 3)
+
+
+#  Data type objects (dtype): Data type objects (dtype) is an example of numpy.dtype class.
+#  It describes how the bytes in the fixed-size block of memory corresponding to an array item should be interpreted.
+sample_array_1 = np.array([[0, 4, 2]])
+
+sample_array_2 = np.array([0.2, 0.4, 2.4])
+
+print("Data type of the array 1 :",
+      sample_array_1.dtype)   #Data type of the array 1 : int64
+
+print("Data type of array 2 :",
+      sample_array_2.dtype) #Data type of array 2 : float64
+
+
+# ToDo: will be starting from different ways of creating arrays
