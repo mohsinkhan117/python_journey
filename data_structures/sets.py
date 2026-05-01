60e7542f5dd0d74b1fffc2d896557c3d2354f2d8
mohsinkhan117 <mohsinkhanmandan@gmail.com>
2026-05-01 10:18:53 -0700

    Done with sets

--- data_structures/sets.py
+++ data_structures/sets.py
@@ -15,9 +15,25 @@ print(type(s))
 s = set(["a", "b", "c"])
 print(s)
 
+
+lst = [1, 2, 2, 3, 3]
+unique = list(set(lst))   #used in real world removing duplicates
+
+text = "this is is a test test"
+words = set(text.split())
+print(words)
+
+
 # Adding element to the set
 s.add("d")
 print(s)
+
+
+a = {1, 2}
+b = {1, 2, 3, 4}
+
+print(a.issubset(b))    # True
+print(b.issuperset(a))  # True
 
 #union of set
 a = {"x", "y"}
@@ -36,8 +52,40 @@ a = {1, 2, 3}
 b = {2, 3, 4}
 d = a.difference(b)
 print(d)
+print(a.symmetric_difference(b))  # {1, 2, 4, 5}
+a.difference_update(b)
+print(a)  # {1}
+
+#short hand methods
+
+print(a | b)  # union
+print(a & b)  # intersection
+print(a - b)  # difference
+print(a ^ b)  # symmetric difference
+
+if set(a) & set(b):
+    print("Common elements exist")
 
 #clear() function removes all elements from a set, leaving it empty.
 s = {1, 2, 3}
 s.clear()
 print(s)
+
+
+
+s.remove(2)     # removes 2
+# s.remove(5)   # ❌ ERROR if not found
+
+s.discard(5)    # ✅ no error
+
+
+a = {1, 2}
+b = {3, 4}
+a.update(b)
+print(a)   # {1, 2, 3, 4}
+
+
+fs = frozenset([1, 2, 3])
+# fs.add(4) ❌ ERROR (immutable)
+
+
