844f70e6b5306f59981dc73cb7189bb4ab8bca76
mohsinkhan117 <mohsinkhanmandan@gmail.com>
2026-05-02 02:10:01 -0700

    done with basic numpy methods

--- data_structures/numpy_arrays.py
+++ data_structures/numpy_arrays.py
@@ -64,4 +64,76 @@ print("Data type of array 2 :",
       sample_array_2.dtype) #Data type of array 2 : float64
 
 
-# ToDo: will be starting from different ways of creating arrays
+# Different ways of creating arrays
+#1. numpy.array(): Numpy array object in Numpy is called ndarray.
+#  We can create ndarray using this function.
+# Syntax: numpy.array(parameter)
+
+
+
+arr = np.array([3,4,5,5])
+
+print("Array :",arr)
+
+#2. numpy.fromiter(): The fromiter() function create a new one-dimensional array 
+# from an iterable object.
+
+#Syntax: numpy.fromiter(iterable, dtype, count=-1)
+
+hello="Hello world"
+arr= np.fromiter(hello,dtype='U2')
+'''what is dtype:
+int32 → integers
+float64 → decimal numbers
+bool → True/False
+U → Unicode strings
+Unlike np.array(), the fromiter() function cannot guess the data type automatically because:
+
+It reads elements one-by-one from an iterator
+It doesn’t know the full dataset in advance
+So it needs explicit instruction on how to store each item'''
+
+#3. numpy.arange(): This is an inbuilt NumPy function that returns evenly spaced values 
+# within a given interval.
+
+# Syntax:  numpy.arange( start , stop, step , dtype=None )
+
+np.arange(1, 20 , 2, 
+          dtype = np.float32)  #array([ 1.,  3.,  5.,  7.,  9., 11., 13., 15., 17., 19.], dtype=float32) 
+
+
+
+#4. numpy.linspace(): This function returns evenly spaced numbers over a specified between two limits. 
+
+# Syntax: numpy.linspace(start, stop, num=50, endpoint=True, retstep=False, dtype=None, axis=0)
+
+np.linspace(3.5, 10, 3, 
+            dtype = np.int32)  #array([ 3,  6, 10], dtype=int32)
+
+
+# 5. numpy.empty(): This function create a new array of given shape and type without initializing value.
+
+# Syntax: numpy.empty(shape, dtype=float, order='C')
+
+print(np.empty([4, 3],
+         dtype = np.int32,
+         order = 'f')
+)
+
+
+# 6. numpy.ones(): This function is used to get a new array of given shape and type filled with ones (1).
+
+# Syntax: numpy.ones(shape, dtype=None, order='C')
+
+np.ones([4, 3],
+        dtype = np.int32,
+        order = 'f')
+
+
+# 7. numpy.zeros(): This function is used to get a new array of given shape and type filled with zeros (0). 
+
+# Syntax: numpy.zeros(shape, dtype=None, order='C')
+
+np.zeros([4, 3],
+        dtype = np.int32,
+        order = 'f')
