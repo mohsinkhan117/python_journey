028ca5ffd83b885b94f7179c3cc22834fa74d490
mohsinkhan117 <mohsinkhanmandan@gmail.com>
2026-05-03 10:53:39 -0700

    starting with Dequeue

---
+++ data_structures/dequeue.py
@@ -0,0 +1,6 @@
+'''
+A deque stands for Double-Ended Queue. It is a special type of data structure that 
+allows to add and remove elements from both ends efficiently. This makes it useful 
+in applications like task scheduling, sliding window problems and real-time data processing.
+'''
+
