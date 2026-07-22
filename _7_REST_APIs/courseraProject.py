1a32429756cf1577f288d6a66502263e430373fb
mohsinkhan117 <mohsinkhanmandan@gmail.com>
2026-07-22 01:44:56 -0700

    found table of interest and extracted data from it

--- _7_REST_APIs/courseraProject.py
+++ _7_REST_APIs/courseraProject.py
@@ -12,24 +12,39 @@ print(bs.title)          # the <title> tag itself
 print(bs.title.string)    # just the text inside <title>
 print(bs.h1.string)              # the first <h1> tag on the page
 print(bs.a) 
-first_table=bs.find("table")
-all_tables=bs.find_all("table")
-print(first_table)
+# first_table=bs.find("table")
+# all_tables=bs.find_all("table")
+# print(first_table)
 
-df=pd.DataFrame(first_table)
+# df=pd.DataFrame(first_table)
 # df=pd.DataFrame(all_tables)
-print(df)
+# print(df)
 
-rows_data = []
-if first_table:
-    rows = first_table.find_all("tr")             # <tr> = table row
-    for row in rows:
-        cells = row.find_all(["td", "th"])   # <td> = data cell, <th> = header cell
-        row_text = [cell.get_text(strip=True) for cell in cells]
-        rows_data.append(row_text)
+# rows_data = []
+# if first_table:
+#     rows = first_table.find_all("tr")             # <tr> = table row
+#     for row in rows:
+#         cells = row.find_all(["td", "th"])   # <td> = data cell, <th> = header cell
+#         row_text = [cell.get_text(strip=True) for cell in cells]
+#         rows_data.append(row_text)
 
-print(rows_data[:3])   # first few rows as plain lists of strings -> team name, year, wins, losses, etc.
+# print(rows_data[:3])   # first few rows as plain lists of strings -> team name, year, wins, losses, etc.
 
-# Turning that manually-parsed data into a DataFrame ourselves
-manual_df = pd.DataFrame(rows_data[1:], columns=rows_data[0] if rows_data else None)
-print(manual_df.head())
+# # Turning that manually-parsed data into a DataFrame ourselves
+# manual_df = pd.DataFrame(rows_data[1:], columns=rows_data[0] if rows_data else None)
+# print(manual_df.head())
+
+
+#pandas can help us reading tables from html directly
+tables=pd.read_html(url)
+print(len(tables))
+for i, table in enumerate(tables):
+    print(f"\n===== TABLE {i} =====")
+    print(table.head())
+
+print(tables[2])
+
+df_table2=pd.DataFrame(tables[2])
+csvtable=df_table2.to_csv("df_table2",index=False)
+# with open as csvtable3:
+#     csvtable3.read(csvtable,"csvtable3")
